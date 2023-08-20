Dounder/nest-rest-template

done: users and auth module

src/users/users.controller.ts
@@ -1,0 +1,50 @@
+import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Patch, Post, Query } from '@nestjs/common';
+
+import { Auth, GetUser } from '../auth/decorators';
+import { PaginationDto } from './../common/dto/pagination.dto';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { UserRole } from './enums/user-role.enum';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  @Auth(UserRole.ADMIN)
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  @Auth(UserRole.ADMIN)
+  findAll(@Query() pagination: PaginationDto) {
+    return this.usersService.findAll(pagination);
+  }
+
+  @Get(':term')
+  @Auth(UserRole.ADMIN)
+  findOne(@Param('term') id: string) {
+    return this.usersService.findOneBy(id);
+  }
+
+  @Patch(':id')
+  @Auth(UserRole.ADMIN)
+  update(@Param('id', ParseUUIDPipe) id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(id, updateUserDto);
+  }
+
+  @Patch(':id/restore')
+  @Auth(UserRole.ADMIN)
+  restore(@Param('id', ParseUUIDPipe) id: string) {
+    return this.usersService.restore(id);
+  }
+
+  @Delete(':id')
+  @Auth(UserRole.ADMIN)
+  remove(@Param('id', ParseUUIDPipe) id: string, @GetUser() user: User) {
+    return this.usersService.remove(user, id);
+  }
+}

Dounder/nest-mongo-rest-boilerplate

src/users/users.controller.ts
@@ -1,0 +1,51 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+
+import { Auth, GetUser } from '../auth/decorators';
+import { PaginationDto } from './../common/dto/pagination.dto';
+import { ParseMongoIdPipe } from './../common/pipes/parse-mongo-id.pipe';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { UserRole } from './enums/user-role.enum';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  @Auth(UserRole.ADMIN)
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  @Auth(UserRole.ADMIN)
+  findAll(@Query() pagination: PaginationDto) {
+    return this.usersService.findAll(pagination);
+  }
+
+  @Get(':term')
+  @Auth(UserRole.ADMIN)
+  findOne(@Param('term') id: string) {
+    return this.usersService.findOneBy(id);
+  }
+
+  @Patch(':id')
+  @Auth(UserRole.ADMIN)
+  update(@Param('id', ParseMongoIdPipe) id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(id, updateUserDto);
+  }
+
+  @Patch(':id/restore')
+  @Auth(UserRole.ADMIN)
+  restore(@Param('id', ParseMongoIdPipe) id: string) {
+    return this.usersService.restore(id);
+  }
+
+  @Delete(':id')
+  @Auth(UserRole.ADMIN)
+  remove(@Param('id', ParseMongoIdPipe) id: string, @GetUser() user: User) {
+    return this.usersService.remove(user, id);
+  }
+}